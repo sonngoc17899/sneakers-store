25403c0aa756a930782e0711527c5a4065e60f7e
SonNgoc <sonngoc17899@gmail.com>
2020-07-30 18:09:43 +0700

    done

--- js/view.js
+++ js/view.js
@@ -249,7 +249,17 @@ view.showInfo = (name, data) =>{
                 </div>
                 <div class="many">
                 <label for="many">Số lượng:</label>
-                <input id="soluong" type="text" placeholder="Nhập số lượng">
+                <select id="soluong">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+            </select>
                 <div class="erros" id="erros-soluong"></div>
                 </div>
             </div>
